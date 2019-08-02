e0e8c592fbf5f3f1b45e663478247198030e5073
DeshmukhChinmay <chinmay2207@gmail.com>
2019-08-02 12:39:17 +1200

    Created an artifact for the project. Added a method to display if there is an argument error

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,24 @@ public class Main {
 
     public static void main(String args[]) {
 
+        System.out.println(args.length);
 
+        if (args.length < 2) {
+            printInputArgumentsError();
+        }
 
+    }
+
+    private static void printInputArgumentsError() {
+        System.out.println("Invalid input");
+        System.out.println("Please run the jar file using the following interface ->\n");
+        System.out.println("java−jar scheduler . jar INPUT.dot P [OPTION]");
+        System.out.println("INPUT. dot   a task graph with integer weights in dot format");
+        System.out.println("P            number of  processors  to  schedule  the INPUT graph on");
+        System.out.println("Optional:");
+        System.out.println("−p N use N cores for execution in parallel (default  is  sequential)");
+        System.out.println("−v visualise the search");
+        System.out.println("−o OUTPUT   output file  is named OUTPUT (default  is INPUT−output.dot)");
     }
 
 }
