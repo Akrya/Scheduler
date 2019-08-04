351fc5976a03a43dde7c3df5e97b3ab4474deacb
Terence Qu <Terenco12345@gmail.com>
2019-08-05 11:44:03 +1200

    Changed SolutionTree and TaskGraph to extend DefaultGraph instead

--- src/data/solutiontree/SolutionTree.java
+++ src/data/solutiontree/SolutionTree.java
@@ -7,6 +7,7 @@ import org.graphstream.graph.Node;
 import org.graphstream.graph.implementations.AbstractEdge;
 import org.graphstream.graph.implementations.AbstractGraph;
 import org.graphstream.graph.implementations.AbstractNode;
+import org.graphstream.graph.implementations.DefaultGraph;
 
 /**
  * 
@@ -15,88 +16,10 @@ import org.graphstream.graph.implementations.AbstractNode;
  * @author Teren
  *
  */
-public class SolutionTree extends AbstractGraph{
+public class SolutionTree extends DefaultGraph{
 
 	public SolutionTree(String id) {
 		super(id);
-	}
-
-	@Override
-	public <T extends Node> T getNode(String id) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public <T extends Node> T getNode(int index) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public <T extends Edge> T getEdge(String id) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public <T extends Edge> T getEdge(int index) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public int getNodeCount() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public int getEdgeCount() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public <T extends Node> Iterator<T> getNodeIterator() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public <T extends Edge> Iterator<T> getEdgeIterator() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	protected void addNodeCallback(AbstractNode node) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	protected void addEdgeCallback(AbstractEdge edge) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	protected void removeNodeCallback(AbstractNode node) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	protected void removeEdgeCallback(AbstractEdge edge) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	protected void clearCallback() {
-		// TODO Auto-generated method stub
-		
 	}
 	
 }
--- src/data/taskgraph/TaskGraph.java
+++ src/data/taskgraph/TaskGraph.java
@@ -6,6 +6,7 @@ import org.graphstream.graph.Node;
 import org.graphstream.graph.implementations.AbstractEdge;
 import org.graphstream.graph.implementations.AbstractGraph;
 import org.graphstream.graph.implementations.AbstractNode;
+import org.graphstream.graph.implementations.DefaultGraph;
 
 
 /**
@@ -16,88 +17,10 @@ import org.graphstream.graph.implementations.AbstractNode;
  * @author Teren
  *
  */
-public class TaskGraph extends AbstractGraph{
+public class TaskGraph extends DefaultGraph{
 
 	public TaskGraph(String id) {
 		super(id);
-	}
-
-	@Override
-	public <T extends Node> T getNode(String id) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public <T extends Node> T getNode(int index) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public <T extends Edge> T getEdge(String id) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public <T extends Edge> T getEdge(int index) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public int getNodeCount() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public int getEdgeCount() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public <T extends Node> Iterator<T> getNodeIterator() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public <T extends Edge> Iterator<T> getEdgeIterator() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	protected void addNodeCallback(AbstractNode node) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	protected void addEdgeCallback(AbstractEdge edge) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	protected void removeNodeCallback(AbstractNode node) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	protected void removeEdgeCallback(AbstractEdge edge) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	protected void clearCallback() {
-		// TODO Auto-generated method stub
-		
 	}
 
 }
