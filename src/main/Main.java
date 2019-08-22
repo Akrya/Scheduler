d2ca369724155fb8ddc87d6de15dd143fe4e6635
kwu849 <kwu849@aucklanduni.ac.nz>
2019-08-22 21:23:51 +1200

    added main scene

--- src/main/Main.java
+++ src/main/Main.java
@@ -137,54 +137,11 @@ public class Main extends Application {
         primaryStage.setTitle("Gantt Chart Sample");
 
         FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(getClass().getClassLoader().getResource("/views/Main.fxml"));
-        Parent root =loader.load();
+        loader.setLocation(getClass().getClassLoader().getResource("views/Main.fxml"));
+        Parent root = loader.load();
         GanttChattController gcontroller = loader.getController();
-//        String[] machines = new String[] { "Machine 1", "Machine 2", "Machine 3" };
-//
-//        final NumberAxis xAxis = new NumberAxis();
-//        final CategoryAxis yAxis = new CategoryAxis();
-//
-//        final GanttChartFX<Number,String> chart;
-//        chart = new GanttChartFX<Number,String>(xAxis,yAxis);
-//        xAxis.setLabel("");
-//        xAxis.setTickLabelFill(Color.CHOCOLATE);
-//        xAxis.setMinorTickCount(4);
-//
-//        yAxis.setLabel("");
-//        yAxis.setTickLabelFill(Color.CHOCOLATE);
-//        yAxis.setTickLabelGap(10);
-//        yAxis.setCategories(FXCollections.<String>observableArrayList(Arrays.asList(machines)));
-//
-//        chart.setTitle("Machine Monitoring");
-//        chart.setLegendVisible(false);
-//        chart.setBlockHeight( 50);
-//        String machine;
-//
-//        machine = machines[0];
-//        XYChart.Series series1 = new XYChart.Series();
-//        series1.getData().add(new XYChart.Data(0, machine, new GanttChartFX.ExtraData( 1, "status-red")));
-//        series1.getData().add(new XYChart.Data(1, machine, new GanttChartFX.ExtraData( 1, "status-green")));
-//        series1.getData().add(new XYChart.Data(2, machine, new GanttChartFX.ExtraData( 1, "status-red")));
-//        series1.getData().add(new XYChart.Data(3, machine, new GanttChartFX.ExtraData( 1, "status-green")));
-//
-//        machine = machines[1];
-//        XYChart.Series series2 = new XYChart.Series();
-//        series2.getData().add(new XYChart.Data(0, machine, new GanttChartFX.ExtraData( 1, "status-green")));
-//        series2.getData().add(new XYChart.Data(1, machine, new GanttChartFX.ExtraData( 1, "status-green")));
-//        series2.getData().add(new XYChart.Data(2, machine, new GanttChartFX.ExtraData( 2, "status-red")));
-//
-//        machine = machines[2];
-//        XYChart.Series series3 = new XYChart.Series();
-//        series3.getData().add(new XYChart.Data(0, machine, new GanttChartFX.ExtraData( 1, "status-blue")));
-//        series3.getData().add(new XYChart.Data(1, machine, new GanttChartFX.ExtraData( 2, "status-red")));
-//        series3.getData().add(new XYChart.Data(3, machine, new GanttChartFX.ExtraData( 1, "status-green")));
-//
-//        chart.getData().addAll(series1, series2, series3);
-//
-//        chart.getStylesheets().add(getClass().getClassLoader().getResource("ganttchart.css").toExternalForm());
 
-        Scene scene  = new Scene(root,620,350);
+        Scene scene  = new Scene(root,620,450);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
