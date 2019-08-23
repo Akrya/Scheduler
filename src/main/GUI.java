a660c7945ee84ebb11e9ae34d321b933b7fb5fdd
kwu849 <kwu849@aucklanduni.ac.nz>
2019-08-23 12:35:36 +1200

    Changed start scene to main

--- src/main/GUI.java
+++ src/main/GUI.java
@@ -1,16 +1,30 @@
 package main;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 import main.controller.GanttChartController;
 
 public class GUI extends Application {
+    private static final String MAIN_MENU_SCENE = "/views/Main.fxml";
+    private static final String APPLICATION_TITLE = "Scheduler";
+
     @Override
     public void start(Stage primaryStage) throws Exception {
-        GanttChartController ganttChartController = GanttChartController.getInstance();
-        ganttChartController.setPrimaryStage(primaryStage);
-        Main.getController().initialise();
-        Main.getController().startGanttVisualise();
+//        GanttChartController ganttChartController = GanttChartController.getInstance();
+//        ganttChartController.setPrimaryStage(primaryStage);
+//        Main.getController().initialise();
+//        Main.getController().startGanttVisualise();
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource(MAIN_MENU_SCENE));
+        Parent root = loader.load();
+
+        // show application
+        primaryStage.setTitle(APPLICATION_TITLE);
+        primaryStage.setScene(new Scene(root, 1200, 800));
+        primaryStage.show();
     }
 
     public static void launchApplication(String args[]) {
--- src/main/controller/GanttChartController.java
+++ src/main/controller/GanttChartController.java
@@ -3,12 +3,9 @@ package main.controller;
 import graph.GraphController;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.fxml.FXML;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
-import javafx.scene.control.Button;
-import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import main.Main;
 
@@ -26,10 +23,8 @@ public class GanttChartController {
     private static GanttChartController ourInstance = new GanttChartController();
 
     private static Stage primaryStage;
-    @FXML
-    static Pane graphPane, chartPane;
-    @FXML
-    static Button graphButton, chartButton;
+
+    private static Scene chart;
 
     final static NumberAxis xAxis = new NumberAxis();
     final static CategoryAxis yAxis = new CategoryAxis();
@@ -71,7 +66,7 @@ public class GanttChartController {
         ganttChart.setLegendVisible(false);
         ganttChart.setBlockHeight(50);
 
-/*        for (int i = 0; i < processors.length; i++) {
+        for (int i = 0; i < processors.length; i++) {
 //            currentXValue = 0;
             XYChart.Series newSeries = new XYChart.Series();
             for (Node n: processors[i].mapOfTasksAndStartTimes.keySet()) {
@@ -85,44 +80,18 @@ public class GanttChartController {
 
         for (XYChart.Series s: seriesList) {
             ganttChart.getData().add(s);
-        }*/
-        String[] machines = new String[] { "Machine 1", "Machine 2", "Machine 3" };
-        String machine;
-
-        machine = machines[0];
-        XYChart.Series series1 = new XYChart.Series();
-        series1.getData().add(new XYChart.Data(0, machine, new GanttChartFX.ExtraData( 1, "status-red")));
-        series1.getData().add(new XYChart.Data(1, machine, new GanttChartFX.ExtraData( 1, "status-green")));
-        series1.getData().add(new XYChart.Data(2, machine, new GanttChartFX.ExtraData( 1, "status-red")));
-        series1.getData().add(new XYChart.Data(3, machine, new GanttChartFX.ExtraData( 1, "status-green")));
-
-        machine = machines[1];
-        XYChart.Series series2 = new XYChart.Series();
-        series2.getData().add(new XYChart.Data(0, machine, new GanttChartFX.ExtraData( 1, "status-green")));
-        series2.getData().add(new XYChart.Data(1, machine, new GanttChartFX.ExtraData( 1, "status-green")));
-        series2.getData().add(new XYChart.Data(2, machine, new GanttChartFX.ExtraData( 2, "status-red")));
-
-        machine = machines[2];
-        XYChart.Series series3 = new XYChart.Series();
-        series3.getData().add(new XYChart.Data(0, machine, new GanttChartFX.ExtraData( 1, "status-blue")));
-        series3.getData().add(new XYChart.Data(1, machine, new GanttChartFX.ExtraData( 1, "status-red")));
-        series3.getData().add(new XYChart.Data(3, machine, new GanttChartFX.ExtraData( 1, "status-green")));
-
-        ganttChart.getData().addAll(series1, series2, series3);
-
-        ganttChart.getStylesheets().add(GanttChartController.class.getClassLoader().getResource("ganttchart.css").toExternalForm());
+        }
 
         ganttChart.getStylesheets().add(Main.class.getClassLoader().getResource("ganttchart.css").toExternalForm());
 
     }
 
-    public static void showStage(Scene scene) {
-        chartPane.getChildren().add(ganttChart);
-        primaryStage.setScene(scene);
+    public static void showStage() {
+        chart = new Scene(ganttChart, 620, 350);
+        primaryStage.setScene(chart);
         primaryStage.show();
     }
 
 
 
 }
-
